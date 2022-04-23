f1d31c6e9b0743730231a569adbe23c9875a610b
nickcrowney <nicholascrowney@gmail.com>
2022-04-23 11:25:29 +0200

    test file added

---
+++ server/testfile3.js
@@ -0,0 +1,27 @@
+const chords = ['C', 'D', 'E'];
+const possibleChords = ['C', 'D', 'E', 'F', 'G'];
+
+const chordsObject = possibleChords.reduce(
+  (a, v) => ({ ...a, [v]: false }),
+  {}
+);
+for (let el of Object.keys(chordsObject)) {
+  console.log(el, 'key');
+  if (chords.includes(el)) {
+    chordsObject[el] = true;
+  }
+  // chordsObject.forEach((el) => {
+}
+// }
+// );
+
+const totalChordsKnow = Object.values(chordsObject);
+console.log(chordsObject);
+console.log(totalChordsKnow);
+
+const totalTrue = Object.values(chordsObject).reduce(
+  (a, chord) => a + (chord === true ? 1 : 0),
+  0
+);
+
+console.log(totalTrue);
